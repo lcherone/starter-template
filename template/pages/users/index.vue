b2a39fcf2a9c25b8b270ed5145b848f696228a28
Lawrence Cherone <lcherone@users.noreply.github.com>
2018-07-25 03:19:31 +0100

    Update index.vue

--- template/pages/users/index.vue
+++ template/pages/users/index.vue
@@ -12,8 +12,7 @@
                     Users
                   </v-flex>
                   <v-flex xs12 sm6>
-                    <v-btn small color="success" @click="dialog = true" style="float:right" :ripple="false">Add User</v-btn>
-                    <v-btn small color="success" @click="prompt()" style="float:right" :ripple="false">Prompt</v-btn>
+                    <v-btn small color="success" @click="prompt()" style="float:right" :ripple="false">Show Prompt</v-btn>
                   </v-flex>
                 </v-layout>
               </v-flex>
@@ -28,76 +27,6 @@
                     </nuxt-link>
                   </li>
                 </ul>
-
-                <!--<v-data-table :headers="headers" :items="items" hide-actions class="elevation-1">-->
-                <!--  <template slot="items" slot-scope="props">-->
-                <!--    <td><a href="javascript:void(0)" @click="authItem(props.item)">{{ props.item.label }}</a></td>-->
-                <!--    <td>{{ props.item.host }}</td>-->
-                <!--    <td>{{ props.item.secret }}</td>-->
-                <!--    <td>-->
-                <!--      <span left v-if="props.item.status && props.item.status.status">-->
-                <!--        <v-icon color="green">check_circle</v-icon>-->
-                <!--        <span>{{ props.item.status.msg }}</span>-->
-                <!--      </span>-->
-                <!--      <span left v-if="props.item.status && !props.item.status.status && props.item.status.msg">-->
-                <!--        <v-icon color="red">error</v-icon>-->
-                <!--        <span>{{ props.item.status.msg }}</span>-->
-                <!--      </span>-->
-                <!--      <span left v-if="!props.item.status || props.item.status && !props.item.status.status && !props.item.status.msg">-->
-                <!--        <v-icon color="orange">error_outline</v-icon>-->
-                <!--        <span>Checking</span>-->
-                <!--      </span>-->
-                <!--    </td>-->
-                <!--    <td class="justify-center layout px-0">-->
-                <!--      <v-tooltip left>-->
-                <!--        <v-btn slot="activator" icon class="mx-0" @click="editItem(props.item)">-->
-                <!--          <v-icon color="teal">edit</v-icon>-->
-                <!--        </v-btn>-->
-                <!--        <span>Edit</span>-->
-                <!--      </v-tooltip>-->
-                <!--      <v-tooltip left>-->
-                <!--        <v-btn slot="activator" icon class="mx-0" @click="deleteItem(props.item)">-->
-                <!--          <v-icon color="pink">delete</v-icon>-->
-                <!--        </v-btn>-->
-                <!--        <span>Delete</span>-->
-                <!--      </v-tooltip>-->
-                <!--    </td>-->
-                <!--  </template>-->
-                <!--  <template slot="no-data">-->
-                <!--    You have not added any servers, add a new server to continue.-->
-                <!--  </template>-->
-                <!--</v-data-table>-->
-                
-                <!--<v-dialog v-model="dialog" max-width="500px">-->
-                <!--  <v-card>-->
-                <!--    <v-card-title>-->
-                <!--      <span class="headline">{{ formTitle }}</span>-->
-                <!--    </v-card-title>-->
-                <!--    <v-card-text>-->
-                <!--      <v-alert outline color="info" icon="info" :value="true">-->
-                <!--        Servers are securely stored in your browser for easy selection.-->
-                <!--      </v-alert>-->
-                <!--      <v-container grid-list-md>-->
-                <!--        <v-form ref="form" v-model="valid" lazy-validation>-->
-                <!--          <v-text-field label="Label" :rules="labelRule" v-model="editedItem.label" required></v-text-field>-->
-                <!--          <v-layout wrap>-->
-                <!--            <v-flex xs12 sm6 md6>-->
-                <!--              <v-text-field label="Host" :rules="hostRule" v-model="editedItem.host" required></v-text-field>-->
-                <!--            </v-flex>-->
-                <!--            <v-flex xs12 sm6 md6>-->
-                <!--              <v-text-field label="Secret" :rules="secretRule" v-model="editedItem.secret" required></v-text-field>-->
-                <!--            </v-flex>-->
-                <!--          </v-layout>-->
-                <!--        </v-form>-->
-                <!--      </v-container>-->
-                <!--    </v-card-text>-->
-                <!--    <v-card-actions>-->
-                <!--      <v-spacer></v-spacer>-->
-                <!--      <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>-->
-                <!--      <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>-->
-                <!--    </v-card-actions>-->
-                <!--  </v-card>-->
-                <!--</v-dialog>-->
               </v-flex>
             </v-layout>
           </v-flex>
@@ -148,7 +77,7 @@ export default {
           closable: false
         },
         title: 'Hi!',
-        text: 'Foo bar baz',
+        text: 'This is a prompt...',
         buttons: [
           {
             title: 'Yes',
